3ea016cdefea3c935452751a20b4d0c0205afedf
Maruf Khan <marufkhan2002@gmail.com>
2026-05-24 11:01:47 +0530

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, Raleway, Lora } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const loraHeading = Lora({subsets:['latin'],variable:'--font-heading'});
+
+const raleway = Raleway({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", raleway.variable, loraHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
